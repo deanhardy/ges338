e90a80021d226952075ba2b8786c85973caa5e5f
Dean Hardy <deanhardy@gmail.com>
2018-09-07 10:32:28 -0400

    added several species

--- scripts/landings.R
+++ scripts/landings.R
@@ -43,28 +43,60 @@ rf <- read.csv('data/landings_rockfish.csv', skip = 4, stringsAsFactors = FALSE)
             dollars = sum(USdollars)) %>%
   mutate(Species = 'Rockfish')
 
-dat <- rbind(bc, eo, rf)
+mh <- read.csv('data/landings_menhaden.csv', skip = 4, stringsAsFactors = FALSE) %>%
+  rename(USdollars = X.) %>%
+  mutate(Year = as.Date(ISOdate(Year, 12, 31)),
+         State = as.character(State),
+         Metric.Tons = as.numeric(Metric.Tons),
+         Pounds = as.numeric(Pounds),
+         USdollars = as.numeric(USdollars)) %>%
+  group_by(Year, State) %>%
+  summarise(metric_tons = sum(Metric.Tons), 
+            pounds = sum(Pounds),
+            dollars = sum(USdollars)) %>%
+  mutate(Species = 'Menhaden')
+
+as <- read.csv('data/landings_shad.csv', skip = 4, stringsAsFactors = FALSE) %>%
+  rename(USdollars = X.) %>%
+  mutate(Year = as.Date(ISOdate(Year, 12, 31)),
+         State = as.character(State),
+         Metric.Tons = as.numeric(Metric.Tons),
+         Pounds = as.numeric(Pounds),
+         USdollars = as.numeric(USdollars)) %>%
+  group_by(Year, State) %>%
+  summarise(metric_tons = sum(Metric.Tons), 
+            pounds = sum(Pounds),
+            dollars = sum(USdollars)) %>%
+  mutate(Species = 'American Shad')
+
+dat <- rbind(bc, eo, rf, mh, as)
+
+
+
+############################################
+## BAY (Rockfish, Oyster, & Crabs)
+############################################
 
 fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'))) +
-  geom_smooth(aes(Year, metric_tons, color = State, linetype = Species)) + 
-  geom_point(aes(Year, metric_tons, color = State, shape = Species)) +
+  geom_line(aes(Year, metric_tons, color = State, linetype = Species)) + 
+  # geom_point(aes(Year, metric_tons, color = State, shape = Species)) +
   scale_x_date(name = 'Year',
                date_breaks = '10 years',
                date_labels = '%Y',
                expand = c(0,0)) +
   scale_y_continuous(name = 'Metric Tons') +
-  ggtitle('Blue Crab, Eastern Oyster, & Rockfish Landings (1950-2016)') +
+  ggtitle('Blue Crab, Eastern Oyster, & Rockfish (Weight) Landings (1950-2016)') +
   theme(panel.background = element_rect(fill = 'white', color = 'black'),
         panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
         panel.grid.major.y = element_blank(),
         panel.grid.minor = element_blank())
 fig
 
-tiff('figures/landings_smoothed.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
+tiff('figures/landings.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
 fig
 dev.off()
 
-## plot US dollars
+## BAY plot US dollars 
 fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'))) +
   geom_line(aes(Year, dollars/1000000, color = State, linetype = Species)) + 
   scale_x_date(name = 'Year',
@@ -79,7 +111,185 @@ fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'))) +
         panel.grid.minor = element_blank())
 fig
 
-tiff('figures/landings_dollars.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
+tiff('figures/dollars.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
 fig
 dev.off()
-  
+
+
+
+######################
+## OYSTER
+######################
+
+fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'), Species == 'Eastern Oyster')) +
+  geom_line(aes(Year, metric_tons, color = State, linetype = Species)) + 
+  # geom_point(aes(Year, metric_tons, color = State, shape = Species)) +
+  scale_x_date(name = 'Year',
+               date_breaks = '10 years',
+               date_labels = '%Y',
+               expand = c(0,0)) +
+  scale_y_continuous(name = 'Metric Tons') +
+  ggtitle('Eastern Oyster Landings (weight; 1950-2016)') +
+  theme(panel.background = element_rect(fill = 'white', color = 'black'),
+        panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor = element_blank())
+fig
+
+tiff('figures/oyster_landings.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
+fig
+dev.off()
+
+## plot US dollars
+fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'), Species == 'Eastern Oyster')) +
+  # geom_line(aes(Year, dollars/1000000, linetype = Species)) + 
+  geom_line(aes(Year, dollars/1000000, color = State, linetype = Species)) + 
+  scale_x_date(name = 'Year',
+               date_breaks = '10 years',
+               date_labels = '%Y',
+               expand = c(0,0)) +
+  scale_y_continuous(name = 'Million Dollars (US$)') +
+  ggtitle('Eastern Oyster Commerical Landings (Dollars; 1950-2016)') +
+  theme(panel.background = element_rect(fill = 'white', color = 'black'),
+        panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor = element_blank())
+fig
+
+tiff('figures/oysters_dollars.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
+fig
+dev.off()
+
+
+
+######################
+## ROCKFISH
+######################
+
+fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'), Species == 'Rockfish')) +
+  geom_line(aes(Year, metric_tons, color = State, linetype = Species)) + 
+  # geom_point(aes(Year, metric_tons, color = State, shape = Species)) +
+  scale_x_date(name = 'Year',
+               date_breaks = '10 years',
+               date_labels = '%Y',
+               expand = c(0,0)) +
+  scale_y_continuous('Metric Tons') +
+  ggtitle('Rockfish Commercial Landings (Weight; 1950-2016)') +
+  theme(panel.background = element_rect(fill = 'white', color = 'black'),
+        panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor = element_blank())
+fig
+
+tiff('figures/rockfish_landings.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
+fig
+dev.off()
+
+## plot US dollars
+fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'), Species == 'Rockfish')) +
+  # geom_line(aes(Year, dollars/1000000, linetype = Species)) + 
+  geom_line(aes(Year, dollars/1000000, color = State, linetype = Species)) + 
+  scale_x_date(name = 'Year',
+               date_breaks = '10 years',
+               date_labels = '%Y',
+               expand = c(0,0)) +
+  scale_y_continuous(name = 'Million Dollars (US$)') +
+  ggtitle('Rockfish Commercial Landings (Dollars; 1950-2016)') +
+  theme(panel.background = element_rect(fill = 'white', color = 'black'),
+        panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor = element_blank())
+fig
+
+tiff('figures/rockfish_dollars.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
+fig
+dev.off()
+
+
+
+######################
+## MENHADEN
+######################
+
+fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'), Species == 'Menhaden')) +
+  geom_line(aes(Year, metric_tons, color = State, linetype = Species)) + 
+  # geom_point(aes(Year, metric_tons, color = State, shape = Species)) +
+  scale_x_date(name = 'Year',
+               date_breaks = '10 years',
+               date_labels = '%Y',
+               expand = c(0,0)) +
+  scale_y_continuous('Metric Tons') +
+  ggtitle('Menhaden Commercial Landings (Weight; 1950-2016)') +
+  theme(panel.background = element_rect(fill = 'white', color = 'black'),
+        panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor = element_blank())
+fig
+
+tiff('figures/menhaden_landings.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
+fig
+dev.off()
+
+## plot US dollars
+fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'), Species == 'Menhaden')) +
+  # geom_line(aes(Year, dollars/1000000, linetype = Species)) + 
+  geom_line(aes(Year, dollars/1000000, color = State, linetype = Species)) + 
+  scale_x_date(name = 'Year',
+               date_breaks = '10 years',
+               date_labels = '%Y',
+               expand = c(0,0)) +
+  scale_y_continuous(name = 'Million Dollars (US$)') +
+  ggtitle('Menhaden Commercial Landings (Dollars; 1950-2016)') +
+  theme(panel.background = element_rect(fill = 'white', color = 'black'),
+        panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor = element_blank())
+fig
+
+tiff('figures/menhaden_dollars.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
+fig
+dev.off()
+
+
+######################
+## AMERICAN SHAD
+######################
+
+fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'), Species == 'American Shad')) +
+  geom_line(aes(Year, metric_tons, color = State, linetype = Species)) + 
+  # geom_point(aes(Year, metric_tons, color = State, shape = Species)) +
+  scale_x_date(name = 'Year',
+               date_breaks = '10 years',
+               date_labels = '%Y',
+               expand = c(0,0)) +
+  scale_y_continuous('Metric Tons') +
+  ggtitle('American Shad Commercial Landings (Weight; 1950-2016)') +
+  theme(panel.background = element_rect(fill = 'white', color = 'black'),
+        panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor = element_blank())
+fig
+
+tiff('figures/shad_landings.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
+fig
+dev.off()
+
+## plot US dollars
+fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'), Species == 'American Shad')) +
+  # geom_line(aes(Year, dollars/1000000, linetype = Species)) + 
+  geom_line(aes(Year, dollars/1000000, color = State, linetype = Species)) + 
+  scale_x_date(name = 'Year',
+               date_breaks = '10 years',
+               date_labels = '%Y',
+               expand = c(0,0)) +
+  scale_y_continuous(name = 'Million Dollars (US$)') +
+  ggtitle('American Shad Commercial Landings (Dollars; 1950-2016)') +
+  theme(panel.background = element_rect(fill = 'white', color = 'black'),
+        panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor = element_blank())
+fig
+
+tiff('figures/shad_dollars.tiff', height = 7, width = 10, units = 'in', res = 300, compression = 'lzw')
+fig
+dev.off()
