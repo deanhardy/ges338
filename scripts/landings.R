3bd515bb1a274f0f9511b246f6a799b45fa666c0
Dean Hardy <deanhardy@gmail.com>
2018-09-07 08:32:52 -0400

    added rockfish

--- scripts/landings.R
+++ scripts/landings.R
@@ -30,17 +30,30 @@ eo <- read.csv('data/landings_easternoyster.csv', skip = 4, stringsAsFactors = F
             dollars = sum(USdollars)) %>%
   mutate(Species = 'Eastern Oyster')
 
-dat <- rbind(bc, eo)
+rf <- read.csv('data/landings_rockfish.csv', skip = 4, stringsAsFactors = FALSE) %>%
+  rename(USdollars = X.) %>%
+  mutate(Year = as.Date(ISOdate(Year, 12, 31)),
+         State = as.character(State),
+         Metric.Tons = as.numeric(Metric.Tons),
+         Pounds = as.numeric(Pounds),
+         USdollars = as.numeric(USdollars)) %>%
+  group_by(Year, State) %>%
+  summarise(metric_tons = sum(Metric.Tons), 
+            pounds = sum(Pounds),
+            dollars = sum(USdollars)) %>%
+  mutate(Species = 'Rockfish')
+
+dat <- rbind(bc, eo, rf)
 
 fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'))) +
   geom_smooth(aes(Year, metric_tons, color = State, linetype = Species)) + 
-  geom_point(aes(Year, metric_tons, color = State)) +
+  geom_point(aes(Year, metric_tons, color = State, shape = Species)) +
   scale_x_date(name = 'Year',
                date_breaks = '10 years',
                date_labels = '%Y',
                expand = c(0,0)) +
   scale_y_continuous(name = 'Metric Tons') +
-  ggtitle('Blue Crab & Eastern Oyster Landings (1950-2017)') +
+  ggtitle('Blue Crab, Eastern Oyster, & Rockfish Landings (1950-2016)') +
   theme(panel.background = element_rect(fill = 'white', color = 'black'),
         panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
         panel.grid.major.y = element_blank(),
@@ -59,7 +72,7 @@ fig <- ggplot(filter(dat, State %in% c('Maryland', 'Virginia'))) +
                date_labels = '%Y',
                expand = c(0,0)) +
   scale_y_continuous(name = 'Million Dollars (US$)') +
-  ggtitle('Blue Crab & Eastern Oyster Landings (1950-2017)') +
+  ggtitle('Blue Crab, Eastern Oyster, & Rockfish Landings (1950-2016)') +
   theme(panel.background = element_rect(fill = 'white', color = 'black'),
         panel.grid.major.x = element_line(color = 'grey', linetype = 'dashed'),
         panel.grid.major.y = element_blank(),
